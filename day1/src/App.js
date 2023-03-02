b09aaeeffccddb4a4dfdf98a21f9a0baa83dbc15
RedVVest <nadolskyy.fake@gmail.com>
2023-03-02 12:53:50 +0200

    project finished

--- day1/src/App.js
+++ day1/src/App.js
@@ -3,7 +3,13 @@ import "./style.css"
 
 function App() {
   return (
-      <div>
+      <div className="app">
+          <div className="number">
+              <div className="one-one"></div>
+              <div className="one-two"></div>
+              <div className="two-one"></div>
+              <div className="two-two"></div>
+          </div>
 
       </div>
   );
