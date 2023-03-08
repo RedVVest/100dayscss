5771a5073bc2681de52cb5f9a5e0f459a341d2ad
RedVVest <nadolskyy.fake@gmail.com>
2023-03-08 09:32:21 +0200

    day 3 project finished

---
+++ day3/src/App.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="circle">
+          <div className="sun"></div>
+          <div className="sky"></div>
+          <div className="left-side"></div>
+          <div className="right-side"></div>
+          <div className="shadow"></div>
+          <div className="sand"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
